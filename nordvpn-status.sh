45d848bcebaa35e09b1edae08793cdf9a8da2d80
Raman Maksimchuk <dotnet044@gmail.com>
2022-12-19 23:29:47 +0300

    Refactor UI rendering

--- nordvpn-status.sh
+++ nordvpn-status.sh
@@ -8,8 +8,8 @@ function timestamp {
   ((sec=seconds%60, seconds/=60, min=seconds%60, hrs=seconds/60))
 
   local plural="s"
-  if ((sec == 1))
-  then plural=""
+  if ((sec == 1)); then
+    plural=""
   fi
 
   if ((hrs == 0 && min == 0)); then
@@ -21,31 +21,91 @@ function timestamp {
   fi
 }
 
-#today=$(date +%c)
+# Prints array elements in format "{index}\t{element}".
+function printArray {
+  local arr=("$@")
+  for i in "${!arr[@]}"; do 
+    printf "%s\t%s\n" "$i" "${arr[$i]}"
+  done
+}
+
+function printStrOfChar {
+  length=$1
+  char=$2
+  if ((length > 0)); then
+    #printf '%*s' "$length" | tr ' ' "$char"
+    printf "$char""%.0s" $(seq 1 $length) #support whitespace char
+  fi
+}
+function printLineOfChar {
+  printStrOfChar $1 $2
+  printf "\n"
+}
+function printCenter {
+  length=$1
+  string=$2
+  diff=$(($length-${#string}))
+  count=$(($diff/2))
+  end=$(($count+$diff%2))
+
+  printStrOfChar $count ' '
+  printf "$lineCommand"
+  printLineOfChar $end ' '
+}
+
 start=$(date +%s)
 started=$(date +%T)
+command="nordvpn status"
 
 while true
 do
+  today=$(date +%c)
+  time=$(date +%T)
+  now=$(date +%s)
+  seconds=$(($now-$start))
+  timestamp $seconds
+ 
+  # Print out all lines for analysis
+  lineCommand=$(echo $command | tr '[:lower:]' '[:upper:]')
+  lineToday=$(echo "Today: $today")
+  lineStarted=$(echo "Started: $started")
+  lineRunning=$(echo "Running: $_timestamp_")
+  lineTime=$(echo "Current time: $time")
+  lines=("$lineCommand" "$lineToday" "$lineStarted" "$lineRunning" "$lineTime")
+  #printArray "${lines[@]}"
+  
+  # Find the longest line
+  declare -i maxLen=0
+  for i in "${!lines[@]}"; do
+    line=${lines[$i]}
+    length=${#line}
+    if ((maxLen < length)); then
+      maxLen=$length
+    fi
+  done
+
+  # Print to terminal window
   clear
-  echo "#########################"
-  echo "#     NORDVPN Status     "
-  echo "#########################"
-  echo "# Today:" $(date +%c)
-  echo "# Started:" $started $'\n'
+  #sharp='#'
+  printLineOfChar $((maxLen+2)) '#'
+  if ((${#lineCommand} >= $maxLen)); then
+    echo \# $lineCommand
+  else
+    printf "# "
+    printCenter $maxLen "$lineCommand"
+  fi
+  printLineOfChar $((maxLen+2)) '#'
+  echo \# $lineToday
+  echo \# $lineStarted
+  printf "#"; printLineOfChar $((maxLen+1)) '~'
+  echo
 
   nordvpn status
   
-  now=$(date +%s)
-  seconds=$(($now-$start))
-
-  timestamp $seconds
-  echo -e "\n# Running:" $_timestamp_
-  
-  t=$(date +%T)
-  echo "# Current time:" $t
-
-  echo "# Press [Ctrl+C] to stop..."
+  echo
+  printf "#"; printLineOfChar $((maxLen+1)) '~'
+  echo \# $lineRunning
+  echo \# $lineTime
+  echo \# Press [Ctrl+C] to stop...
   sleep 1
 done
-
